f0b58aa8c9ae8215fbc1b55fdd067f706f1e27a4
Jesus Reyes <djrm1204@gmail.com>
2022-03-05 00:33:47 -0400

    Rutas y controllador para modelo Album

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+use Illuminate\Support\Facades\Cache;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cache::remember('albums-list', 60 * 60, function () {
+            return Album::all();
+        });
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAlbumRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAlbumRequest $request)
+    {
+        $album = Album::create($request->validated());
+        Cache::forget('albums-list');
+        return $album;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAlbumRequest  $request
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAlbumRequest $request, Album $album)
+    {
+        $album->update($request->validated());
+        Cache::forget('albums-list');
+        return $album;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        $album->delete();
+        Cache::forget('albums-list');
+        return response(['message' => 'Album deleted'], 204);
+    }
+}
